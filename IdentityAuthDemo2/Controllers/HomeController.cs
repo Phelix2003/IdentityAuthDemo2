d5eb5c68fc2c6e073d0a354f7cdb6fea277d963b
Jonathan <jha@bea.be>
2018-11-11 17:29:00 +0100

    Updated on part one of the project

---
+++ IdentityAuthDemo2/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+// http://johnatten.com/2014/06/22/asp-net-identity-2-0-customizing-users-and-roles/#Core-Identity-2-0-Objects-are-Generic
+namespace IdentitySample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Authorize]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
